227bfaa55f941efe78c8d8a814798b5221549235
Isaac <Isaacformato@gmail.com>
2025-06-12 17:14:05 -0400

    basic transaction table setup

--- bank.sql
+++ bank.sql
@@ -1,11 +1,16 @@
-CREATE TABLE users (
-	user_id serial primary key,
-	first_name varchar(45),
-	last_name varchar(45)
+CREATE TABLE Users (
+	user_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(45),
+	last_name VARCHAR(45),
+	balance INT
 );
 
-CREATE TABLE transations (
-
+CREATE TABLE Transations (
+	transaction_id SERIAL PRIMARY KEY,
+	from_user_id int REFERENCES users(user_id),
+	to_user_id INT REFERENCES users(user_id),
+	sent_num INT,
+	date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
