99eeb08238b901cb26746c188da4966bd0935000
Isaac <Isaacformato@gmail.com>
2025-06-12 17:52:15 -0400

    very basic role integration for users

--- bank.sql
+++ bank.sql
@@ -2,15 +2,17 @@ CREATE TABLE Users (
 	user_id SERIAL PRIMARY KEY,
 	first_name VARCHAR(45),
 	last_name VARCHAR(45),
-	balance INT
+	balance INT,
+	date_created DATE,
+	role VARCHAR(10) CHECK(role IN('Empl', 'User', 'Admin'))
 );
 
-CREATE TABLE Transations (
+CREATE TABLE Transactions (
 	transaction_id SERIAL PRIMARY KEY,
 	from_user_id int REFERENCES users(user_id),
 	to_user_id INT REFERENCES users(user_id),
-	sent_num INT,
-	date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	amount FLOAT,
+	date_sent DATE
 );
 
 
