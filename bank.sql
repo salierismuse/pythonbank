948c27d5844d5b4cf2e67d5b60f9203847c32cc4
salierismuse <isaacformato@Gmail.com>
2025-06-17 00:22:06 -0400

    slight table update, new functions, test cases

--- bank.sql
+++ bank.sql
@@ -1,8 +1,12 @@
 CREATE TABLE Users (
 	user_id SERIAL PRIMARY KEY,
-	first_name VARCHAR(45),
-	last_name VARCHAR(45),
-	balance INT,
+	first_name TEXT,
+	last_name TEXT,
+	street TEXT,
+	city TEXT,
+	state TEXT,
+	zip_code TEXT,
+	balance DECIMAL(10, 2),
 	date_created DATE,
 	role VARCHAR(10) CHECK(role IN('Empl', 'User', 'Admin'))
 );
@@ -11,7 +15,7 @@ CREATE TABLE Transactions (
 	transaction_id SERIAL PRIMARY KEY,
 	from_user_id int REFERENCES users(user_id),
 	to_user_id INT REFERENCES users(user_id),
-	amount FLOAT,
+	amount DECIMAL(10, 2),
 	date_sent DATE
 );
 
--- database.py
+++ database.py
@@ -1,17 +1,34 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname=postgres user=postgres password=****")
+conn = psycopg2.connect("dbname=postgres user=postgres password=amadeus")
 cur = conn.cursor()
 
-# cur.execute("SELECT * FROM testing;")
-# cur.fetchone()
-
-
-def make_user(first_name, last_name, balance, role):
-    cur.execute("INSERT INTO Users (first_name, last_name, balance, role) VALUES (%s, %s, %s, %s)", (first_name, last_name, balance, role))
+# create user!
+def make_user(user_data):
+    cur.execute("INSERT INTO Users (first_name, last_name, balance, role) VALUES (%s, %s, %s, %s)", (user_data[0], user_data[1], user_data[2], user_data[3]))
     conn.commit()
 
-make_user("olivia", "fortnite", 2, "User")
+#user look-up based on id
+def find_user(user_id):
+    cur.execute("SELECT * FROM Users WHERE user_id = %s;", (user_id))
+    return cur.fetchone()
+
+#deleting user based on id
+def delete_user(user_id):
+    if (find_user(user_id)):
+        cur.execute("DELETE FROM Users WHERE user_id = %s", (user_id))
+        conn.commit()
+
+def transaction(transact_data):
+    return 0
+
+
+def test_cases():
+    val = find_user("6")
+    print(val)
+    delete_user("6")
+    val = find_user("6")
+    print(val)
 
 cur.close()
 conn.close()
